d79ed8e9e4de6240f1fff4b3ee0e90bb40b73f9b
Jared Wasinger <j-wasinger@hotmail.com>
2017-11-30 23:40:57 +0000

    asfd

--- tests/EWASMStateTestsRunner.js
+++ tests/EWASMStateTestsRunner.js
@@ -3,11 +3,10 @@ const VM = require('../index.js')
 const testUtil = require('./util')
 const Trie = require('merkle-patricia-tree/secure')
 const ethUtil = require('ethereumjs-util')
-const evm2wast = require('evm2wasm').evm2wast
-
 const BN = ethUtil.BN
+const evm2wasm = require('evm2wasm').evm2wasm
 
-function parseTestCases (forkConfig, testData, data, gasLimit, value) {
+async function parseTestCases (forkConfig, testData, data, gasLimit, value) {
   let testCases = testData['post'][forkConfig].map(testCase => {
     let testIndexes = testCase['indexes']
     let tx = Object.assign({}, testData.transaction)
@@ -23,9 +22,18 @@ function parseTestCases (forkConfig, testData, data, gasLimit, value) {
       return null
     }
 
-    tx.data = testData.transaction.data[testIndexes['data']]
+		// convert transaction data to wasm
+    testData.transaction.data[testIndexes['data']]
+		let txData = Buffer.from(testData.transaction.data[testIndexes['data']].slice(2), 'hex')
+
+		tx.data = await evm2wasm(txData)
+
     tx.gasLimit = testData.transaction.gasLimit[testIndexes['gas']]
     tx.value = testData.transaction.value[testIndexes['value']]
+
+    //convert pre-state account code to wasm
+		debugger
+
     return {
       'transaction': tx,
       'postStateRoot': testCase['hash'],
@@ -41,18 +49,74 @@ function parseTestCases (forkConfig, testData, data, gasLimit, value) {
   return testCases
 }
 
-async function runTestCase (options, testData, t, cb) {
-  for (let preAccount in testData.pre) {
-    preAccount = testData.pre[preAccount]
+function runTestCase (options, testData, t, cb) {
+  const state = new Trie()
+  let block, vm
 
-    if (preAccount && preAccount.code) {
-      //let wast = await evm2wast(preAccount.code.slice(2))   
-      let code = 'f1'
-      let wast = await evm2wast(Buffer.from(code, 'hex'))   
-      debugger
+  async.series([
+    function (done) {
+      vm = new VM({
+        state: state
+      })
+      testUtil.setupPreConditions(state, testData, done)
+    },
+    function (done) {
+      var tx = testUtil.makeTx(testData.transaction)
+      block = testUtil.makeBlockFromEnv(testData.env)
+      tx._homestead = true
+      tx.enableHomestead = true
+      block.isHomestead = function () {
+        return true
+      }
+
+      if (tx.validate()) {
+        if (options.jsontrace) {
+          vm.on('step', function (e) {
+            let hexStack = []
+            hexStack = e.stack.map(item => {
+              return '0x' + new BN(item).toString(16, 0)
+            })
+
+            var opTrace = {
+              'pc': e.pc,
+              'op': e.opcode.opcode,
+              'gas': '0x' + e.gasLeft.toString('hex'),
+              'gasCost': '0x' + e.opcode.fee.toString(16),
+              'stack': hexStack,
+              'depth': e.depth,
+              'opName': e.opcode.name
+            }
+
+            console.log(JSON.stringify(opTrace))
+          })
+        }
+        vm.runTx({
+          tx: tx,
+          block: block
+        }, function (err, r) {
+          err = null
+          done()
+        })
+      } else {
+        done()
+      }
+    },
+    function (done) {
+      if (testData.postStateRoot.substr(0, 2) === '0x') {
+        testData.postStateRoot = testData.postStateRoot.substr(2)
+      }
+      t.equal(state.root.toString('hex'), testData.postStateRoot, 'the state roots should match')
+
+      if (state.root.toString('hex') !== testData.postStateRoot.toString('hex')) {
+        // since General State Tests, postState keys are no longer included in
+        // the state test format. only postStateRoot, so can't debug expected post conditions
+        // testUtil.verifyPostConditions(state, testData.post, t, done)
+        done()
+      } else {
+        done()
+      }
     }
-  }
-  return cb()
+  ], cb)
 }
 
 module.exports = function runStateTest (options, testData, t, cb) {
